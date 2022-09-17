894b4b331067f6b5c273e97b4949c65d4a48085f
Oana4 <sirbu.oana4@gmail.com>
2022-09-17 18:16:09 +0300

    task 2.1 added

---
+++ homework_2/task1.py
@@ -0,0 +1,9 @@
+my_name = 'Oana'
+day = 'Saturday'
+
+# one option:
+print(f'Good day, {my_name}! {day} is a perfect day to learn some python.')
+
+# another option:
+print('Good day, %s! %s is a perfect day to learn some python.' % (my_name, day))
+
