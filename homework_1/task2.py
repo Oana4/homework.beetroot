fe2cb84f0601d081deac87f5da5a2309abf27171
Oana4 <sirbu.oana4@gmail.com>
2022-09-17 17:40:33 +0300

    task 2 added

---
+++ homework_1/task2.py
@@ -0,0 +1,8 @@
+# Here is my task solved for the first lesson
+
+# 17/09/2022
+print('17', '09', '2022', sep='/')
+
+# You should enter your e-mail address as follows: name@domain.com
+print('You', 'should', 'enter', 'your', 'e-mail', 'address', 'as', 'follows', sep=' ', end=':')
+print(' name', 'domain.com', sep='@')
