ab39bf013b1da08abc1cdf1722313fbedc506e24
Oana4 <sirbu.oana4@gmail.com>
2022-09-17 22:13:02 +0300

    task 4.2 added

---
+++ homework_4/task2.py
@@ -0,0 +1,4 @@
+name = input("Please enter your first name: ")
+age = int(input('Please, enter your age: '))
+
+print("Hello %s, on your next birthday you'll be %d years old!" % (name.capitalize(), age+1))
