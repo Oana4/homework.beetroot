81b0910823aad4b2d94cc7a320d41644b681f0cf
Oana4 <sirbu.oana4@gmail.com>
2022-09-17 22:52:45 +0300

    task 4.4 added

---
+++ homework_4/task4.py
@@ -0,0 +1,9 @@
+print('What is the area of a rectangle with L = 3 and l = 2?')
+right_answer = 6
+
+user_answer = int(input('Your answer to this question is (please type an integer): '))
+
+if user_answer == right_answer:
+    print("Well done!")
+else:
+    print("It seems you've made a mistake")
