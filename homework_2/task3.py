3899bf51fd562223906088111f18f883801acb55
Oana4 <sirbu.oana4@gmail.com>
2022-09-17 18:29:05 +0300

    task 2.3 added

---
+++ homework_2/task3.py
@@ -0,0 +1,23 @@
+a = 12
+b = 21
+
+# addition
+print(a + b)
+
+# subtraction
+print(a - b)
+
+# division
+print(a / b)
+
+# multiplication
+print(a * b)
+
+# exponent
+print(a ** b)
+
+# modulus
+print(a % b)
+
+# floor division
+print(a // b)
