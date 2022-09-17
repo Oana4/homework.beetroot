f5ca6a9a7071f9d4fabc8ed61b369bdbf3c1d3d8
Oana4 <sirbu.oana4@gmail.com>
2022-09-17 18:01:46 +0300

    task 3 added

---
+++ homework_1/task3.py
@@ -0,0 +1,8 @@
+# print capital O
+print('#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########')
+
+# leave 2 blank spaces between the letters
+print('\n\n')
+
+# print capital H
+print('#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#\n')
