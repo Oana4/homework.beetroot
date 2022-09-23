f4ffa79014522bd67ad0723f1e1ccb8a6f1d3d31
Oana Sirbu <112729536+Oana4@users.noreply.github.com>
2022-09-23 16:11:08 +0300

    Update sort_reverse_list.py

--- classwork/sort_reverse_list.py
+++ classwork/sort_reverse_list.py
@@ -1,3 +1,8 @@
+# You have a first list like values = [“one”, “two”, “three”, “four”, “five”] 
+# and a second list indexes [1, 3, 5]. 
+# You need to remove elements from first list which have an indexes from a second.
+
+
 list_values = ['one', 'two', 'three', 'four', 'five', 'six']
 list_indexes = [1, 5, 3]
 
