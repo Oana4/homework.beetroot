efe3abb143f40d467554d19274ecb9109148b7d7
Oana4 <sirbu.oana4@gmail.com>
2022-09-23 19:59:12 +0300

    simple sqrt function

---
+++ addtitional_tasks/simple_sqrt_func.py
@@ -0,0 +1,7 @@
+# Create a program that reads from the user input int and prints back the square root of this number.
+from math import sqrt
+
+n = input("Input some integer variable, please: ")
+
+print(sqrt(float(n)))
+
