ed66ef2aed1dfe45abbefa20da8b96a9e679ffb0
Oana4 <sirbu.oana4@gmail.com>
2022-09-26 00:18:26 +0300

    split a list every nth elem

---
+++ addtitional_tasks/split_at_nth.py
@@ -0,0 +1,12 @@
+# Write a Python program to split a list every Nth element.
+# Sample list: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
+# Expected Output: [['a', 'd', 'g', 'j', 'm'], ['b', 'e', 'h', 'k', 'n'], ['c', 'f', 'i', 'l']]
+
+sample_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
+
+
+def split_list(any_list, n):
+    return [any_list[i::n] for i in range(n)]
+
+
+print(split_list(sample_list, 3))
