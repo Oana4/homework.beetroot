3fda6c00926ec5e55e4b2696e6dc51379aebbc3a
Oana4 <sirbu.oana4@gmail.com>
2022-09-29 17:50:31 +0300

    task 11.4 added

---
+++ homework_11/task4.py
@@ -0,0 +1,12 @@
+class CustomException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+        with open("logs.txt", "w") as log_file:
+            log_file.write(msg)
+
+
+try:
+    raise CustomException("You will find this error in the logs.txt file :)")
+except CustomException as my_err:
+    print(my_err)
+
