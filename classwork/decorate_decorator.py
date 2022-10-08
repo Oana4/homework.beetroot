8faf3d03781bac52fc5f23c3316666f6d6f0cc87
Oana4 <sirbu.oana4@gmail.com>
2022-10-08 14:16:27 +0300

    decorate decorator

---
+++ classwork/decorate_decorator.py
@@ -0,0 +1,31 @@
+# Create a decorator for printing logs about when a function starts to run. After it - create a decorator
+# which will convert the result of the function to some format, like “Result of function is {something}”.
+# And put the function like “test_function_something” into two decorators in a row.
+
+from datetime import datetime
+
+
+def log_decorator(func):
+    def wraps(*args):
+        print("Function started at: ", datetime.now())
+        return func(*args)
+
+    return wraps
+
+
+@log_decorator
+def format_decorator(func):
+    def wraps(*args):
+        result = func(*args)
+        return f"The result of the function is {result}"
+
+    return wraps
+
+
+# @log_decorator
+@format_decorator
+def some_function_test():
+    return 33
+
+
+print(some_function_test())
