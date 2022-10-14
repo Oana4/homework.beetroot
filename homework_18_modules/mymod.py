b1449d191d98cf43e4f0c9208df35f89226277bf
Oana4 <sirbu.oana4@gmail.com>
2022-10-14 23:09:51 +0300

    homework 18 v.2

---
+++ homework_18_modules/mymod.py
@@ -0,0 +1,29 @@
+# this is the 3rd task
+import os
+
+
+def count_lines(name):
+    if not os.path.exists(name):
+        return OSError("The path doesn't exist!")
+    with open(name) as file:
+        counter = file.readlines()
+    return len(counter)
+
+
+def count_chars(name):
+    if not os.path.exists(name):
+        return OSError("The path doesn't exist!")
+    with open(name) as file:
+        c_counter = file.read()
+    return len(c_counter)
+
+
+def test(name):
+    if not os.path.exists(name):
+        return OSError("The path doesn't exist!")
+    return f"For the number of lines we call count_lines and we get {count_lines(name)} lines, " \
+           f"and meanwhile by calling count_chars we get {count_chars(name)} characters in {name} file."
+
+
+print(test("mymod.py"))
+
