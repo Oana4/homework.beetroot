c3114fae4d38ed5566d7ac4b419819e29fc728ab
Oana4 <sirbu.oana4@gmail.com>
2022-10-15 12:13:25 +0300

    homework 18 v.3

---
+++ homework_18_modules/mymod_v.2.ambitious.py
@@ -0,0 +1,17 @@
+# this is the 3rd task
+import os
+
+
+def test(name):
+    if not os.path.exists(name):
+        return OSError("The path doesn't exist!")
+    with open(name) as file:
+        counter = len(file.readlines())
+        file.seek(0)
+        c_counter = len(file.read())
+    return f"For the number of lines we get {counter} lines, " \
+           f"and meanwhile we get {c_counter} characters in {name} file."
+
+
+print(test("mymod_v.2.ambitious.py"))
+
