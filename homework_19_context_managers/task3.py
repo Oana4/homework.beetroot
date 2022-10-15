17587c3b02c80d5b493952f2285cf2f527914ceb
Oana4 <sirbu.oana4@gmail.com>
2022-10-15 20:29:55 +0300

    task 19.3 added

---
+++ homework_19_context_managers/task3.py
@@ -0,0 +1,27 @@
+import pytest
+from task1 import OpenFile
+
+
+# I'll use "fixtures can request other fixtures"
+# Some nice theory: in this code, test_data “requests” check and when pytest sees this, it will execute
+# the check fixture function and pass the object it returns into test_data as the check argument.
+@pytest.fixture
+def replace_file_obj():
+    return "new_test.txt"
+
+
+@pytest.fixture
+def check(replace_file_obj):
+    with OpenFile(replace_file_obj, 'r') as opened_file:
+        data = opened_file.read()
+        return data[:10]
+
+
+def test_data(check):
+    assert check == "this is al"
+
+# code output:
+# task3.py::test_data PASSED                                               [100%]
+#
+# ============================== 1 passed in 0.01s ===============================
+
