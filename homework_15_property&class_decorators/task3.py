81da34aca372cfefff4d2a6514f0b4a0356cfcaf
Oana4 <sirbu.oana4@gmail.com>
2022-10-16 21:02:30 +0300

    task 15.3 added

---
+++ homework_15_property&class_decorators/task3.py
@@ -0,0 +1,30 @@
+from functools import wraps
+
+
+class TypeDecorators:
+
+    @staticmethod
+    def to_type(_type):
+        def inner_to_type(func):
+            @wraps(func)
+            def decorator_to_int(*args):
+                try:
+                    return _type(func(*args))
+                except ValueError(f"The result cannot be converted to integer") as err:
+                    raise err
+            return decorator_to_int
+        return inner_to_type
+
+
+@TypeDecorators.to_type(int)
+def do_nothing(string: str):
+    return string
+
+
+@TypeDecorators.to_type(bool)
+def do_something(string: str):
+    return string
+
+
+assert do_nothing('25') == 25
+assert do_something('True') is True
