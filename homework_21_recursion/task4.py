6ddf208450c1c122e061c600dcb54c5043c5ce85
Oana4 <sirbu.oana4@gmail.com>
2022-10-21 22:32:43 +0300

    task 21.4 added

---
+++ homework_21_recursion/task4.py
@@ -0,0 +1,11 @@
+# Function returns reversed input string
+def reverse(input_str: str) -> str:
+    if len(input_str) == 0:
+        return ''
+    else:
+        return input_str[-1] + f"{reverse(input_str[:-1])}"
+
+
+assert reverse("hello") == "olleh"
+assert reverse("o") == "o"
+print(reverse("oana"))
