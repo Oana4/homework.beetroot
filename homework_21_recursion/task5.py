5ee4943e62654ab2f7d875fd9681458794122c1c
Oana4 <sirbu.oana4@gmail.com>
2022-10-22 13:53:46 +0300

    task 21.5 added

---
+++ homework_21_recursion/task5.py
@@ -0,0 +1,20 @@
+def sum_of_digits(digit_string: str) -> int:
+    if not digit_string.isdigit():
+        raise ValueError("input string must be digit string")
+    else:
+        digit_string = int(digit_string)
+
+    if digit_string == 0:
+        return 0
+    else:
+        return digit_string % 10 + sum_of_digits(str(digit_string // 10))
+
+    # if len(digit_string) == 0:
+    #     return 0
+    # else:
+    #     return int(digit_string[0]) + sum_of_digits(digit_string[1:])
+
+
+assert sum_of_digits('26') == 8
+assert sum_of_digits('1234') == 10
+# assert sum_of_digits('test') == ValueError("input string must be digit string")
