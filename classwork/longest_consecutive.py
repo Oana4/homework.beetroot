9094159e663f128b603ffb056d6688104ee334d5
Oana4 <sirbu.oana4@gmail.com>
2022-11-03 21:15:33 +0200

    update

--- classwork/longest_consecutive.py
+++ classwork/longest_consecutive.py
@@ -1,9 +1,9 @@
 # # Write a function that takes on an input random ints (between 1 and 10) and
 # # returns the longest consecutive run and the length of that run of elements of the list.
 #
-# Ex.     def task1(1,2,3,4,4,4,4,4,4,5,6,7,4,2,2,5) -> 6, 4
-#     def task1(1) -> 1, 1
-#     def task1() -> 0, None
+# Ex.     def task1.py(1,2,3,4,4,4,4,4,4,5,6,7,4,2,2,5) -> 6, 4
+#     def task1.py(1) -> 1, 1
+#     def task1.py() -> 0, None
 #
 # Then create another function which takes on input result of function from the previous step and
 # prints Informative message about the longest consecutive run, something like - “Longest run is 6 of integers - 4”
