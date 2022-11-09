1e5616ee8f797dac793b28d156bbd13058229dc2
Oana4 <sirbu.oana4@gmail.com>
2022-11-09 22:25:52 +0200

    employees container with iter

--- custom_homework/8.11 - bugged code for employees container.py
+++ custom_homework/8.11 - bugged code for employees container.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import time
 
 
 class Employee:
-    def __init__(self, name, age, position, salary_per_hour):
+    def __init__(self, name, age, position):
         self.name = name
         self.age = age
         self.position = position
@@ -13,31 +13,30 @@ class Employee:
     def __repr__(self):
         return f"Name: {self.name}, hourly salary: {self.salary_per_hour}"
 
-    def start_working(self):
+    def start_working():
         self.started_pass = datetime.now()
 
     def finish_working(self):
         if self.started_pass != 0:
             difference = datetime.now() - self.started_pass
             self.hours_worked += difference.total_seconds() / 3600
-            self.started_pass = 0
 
     def calculate_salary(self):
         return self.hours_worked * self.salary_per_hour
 
 
-class ListOfEmployees:
+class ListOfEmployees(Employee):
     list_of_employees = []
 
-    @classmethod
+    @staticmethod
     def add_employee(cls):
         name = input("What is your employee's name? ")
         age = int(input("What is your employee's age? "))
         position = input("What is your employee's position? ")
-        salary_per_hour = float(input("What is your employee's hourly salary? "))
+        salary_per_hour = input("What is your employee's hourly salary? ")
 
         new_employee = Employee(name, age, position, salary_per_hour)
-        cls.list_of_employees.append(new_employee)
+        cls.list_of_employees.add(new_employee)
         return new_employee
 
     def __iter__(self):
@@ -47,8 +46,8 @@ class ListOfEmployees:
     def __next__(self):
         if self.index < len(self.list_of_employees):
             temp_index = self.index
-            self.index += 1
-            return self.list_of_employees[temp_index]
+            self.index = self.index + 1
+            print(self.list_of_employees[self.index])
         else:
             raise StopIteration
 
