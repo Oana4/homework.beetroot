a587f71e22a74393a7b97b6fa5dc6b33a304d51f
Oana4 <sirbu.oana4@gmail.com>
2022-12-06 22:26:49 +0200

    double linked lists updated

--- custom_homework/double_linked_list.py
+++ custom_homework/double_linked_list.py
@@ -6,7 +6,7 @@ class Node:
         self.next = next_node
 
     def __str__(self):
-        return self.value
+        return str(self.value)
 
 
 class LinkedList:
@@ -49,17 +49,47 @@ class LinkedList:
             node.next = self.head
             self.head = node
 
+    def add_node(self, value):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+        elif value < self.head.value:
+            node.next = self.head
+            self.head = node
+        else:
+            current = self.head
+            previous = None
+            while current is not None:
+                if current.next is None:
+                    current.next = node
+                    break
+                elif value < current.next.value:
+                    node.next = current.next
+                    current.next = node
+                    break
+                elif value > current.next.value:
+                    previous = current
+                    current = current.next
+            else:
+                previous.next = node
+                node.next = None
+
 
 my_linked_list = LinkedList()
-print(my_linked_list.head)  # None
-print(my_linked_list.tail)  # None
-print(my_linked_list.num_of_list_items)     # 0
+# print(my_linked_list.head)  # None
+# print(my_linked_list.tail)  # None
+# print(my_linked_list.num_of_list_items)     # 0
 
-my_linked_list.add_tail_node("holiday")
-my_linked_list.add_tail_node("winter")
-my_linked_list.add_tail_node("favorite season")
+# my_linked_list.add_tail_node("holiday")
+# my_linked_list.add_tail_node("winter")
+# my_linked_list.add_tail_node("favorite season")
+#
+# my_linked_list.add_head_node("Christmas")
 
-my_linked_list.add_head_node("Christmas")
+my_linked_list.add_node(2)
+my_linked_list.add_node(3)
+my_linked_list.add_node(4)
+my_linked_list.add_node(1)
 
 for item in my_linked_list:
     print(item)
